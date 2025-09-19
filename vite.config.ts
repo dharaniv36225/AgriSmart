292c4d07d6a427318141971e6285b3ad4044167a
dharaniv36225 <dharaniv004@gmail.com>
2025-09-19 08:00:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AgriSmart/',   // 👈 important for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
